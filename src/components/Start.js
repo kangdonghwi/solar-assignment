7ae40e795298041edf28f917f9e0212f02f656e3
KANGDONGHWI <rkdehdgnl@naver.com>
2021-08-18 08:11:00 +0900

    Delete: 콘솔 제거

--- src/components/Start.js
+++ src/components/Start.js
@@ -3,12 +3,10 @@ import styled from "styled-components";
 function Start({ setBool }) {
   const onClick = () => {
     setBool(true);
-    console.log("클릭");
   };
 
   return (
     <StartContainer>
-      {" "}
       <button onClick={onClick}>시작 버튼</button>
     </StartContainer>
   );

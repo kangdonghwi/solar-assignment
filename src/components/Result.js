3e311d47a0f84596759bfb69588957dc6338f1b8
KANGDONGHWI <rkdehdgnl@naver.com>
2021-08-18 08:38:53 +0900

    Modify: 퀵정렬 utils로 이동

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,31 +1,14 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-
+import QuickSort from "../utils/QuickSort";
 function Result({ arr }) {
   const [time, setTime] = useState();
 
-  const quickSort = (arr) => {
-    if (arr.length <= 1) {
-      return arr;
-    }
-
-    const pivot = arr[0];
-    const left = [];
-    const right = [];
-
-    for (let i = 1; i < arr.length; i++) {
-      if (pivot > arr[i]) {
-        left.push(arr[i]);
-      } else {
-        right.push(arr[i]);
-      }
-    }
-    return quickSort(left).concat(pivot, quickSort(right));
-  };
+  
 
   const timeout = () => {
     setTimeout(() => {
-      return setTime(quickSort(arr).reverse().join());
+      return setTime(QuickSort(arr).reverse().join());
     }, 3000);
   };
 
@@ -35,7 +18,7 @@ function Result({ arr }) {
 
   return (
     <ResultContainer>
-      <div> 결과 오름차순 : {quickSort(arr).join()} </div>
+      <div> 결과 오름차순 : {QuickSort(arr).join()} </div>
       <br />
       <div> 결과 내림차순 : {time} </div>
     </ResultContainer>
---
+++ src/utils/QuickSort.js
@@ -0,0 +1,29 @@
+export default function QuickSort(array, left = 0, right = array.length - 1) {
+  if (left >= right) {
+    return;
+  }
+  const mid = Math.floor((left + right) / 2);
+  const pivot = array[mid];
+  const partition = divide(array, left, right, pivot);
+  QuickSort(array, left, partition - 1);
+  QuickSort(array, partition, right);
+  function divide(array, left, right, pivot) {
+    while (left <= right) {
+      while (array[left] < pivot) {
+        left++;
+      }
+      while (array[right] > pivot) {
+        right--;
+      }
+      if (left <= right) {
+        let swap = array[left];
+        array[left] = array[right];
+        array[right] = swap;
+        left++;
+        right--;
+      }
+    }
+    return left;
+  }
+  return array;
+}

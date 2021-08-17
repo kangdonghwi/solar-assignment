e6b05f62ad932d90ba918d094ec60eb454f4f3ac
KANGDONGHWI <rkdehdgnl@naver.com>
2021-08-17 17:45:10 +0900

    Feat: 키코드로직 추가

---
+++ src/utils/CheckKeyCode.js
@@ -0,0 +1,24 @@
+export default function CheckKeyCode(event){
+	let code = event.keyCode;
+	try{
+		if((code === 188 )){
+			return
+		}
+		else if(event.ctrlKey) {
+			return;
+		}
+		else if (code === 9 || code === 36 || code === 35 || code === 37 ||
+			code === 39 || code === 8 || code === 46 || code === 16) {
+		return;
+		}
+		else if(code >=96 && code<=105){
+			return;
+		}
+		else if ((code < 48) || (code > 57)) 
+			throw 'NaN'
+	}catch(err){
+		if(err === 'NaN'){
+			alert('숫자를 입력하세요.');
+		}
+	}
+}
